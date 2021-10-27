e47ba1149be01740b62d106de32c43c43168a667
tomato-commits <arthurpeter7@gmail.com>
2021-10-27 19:27:50 +0300

    subtask 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// 1. Create and render picture gallery
+const gallery = document.querySelector('.gallery');
+
+createGalleryItems();
+
+function createGalleryItems() {
+    const markup = galleryItems
+        .map((image) => {
+            return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                    <img
+                        class="gallery__image"
+                        src="${image.preview}"
+                        data-source="${image.original}"
+                        alt="${image.description}"
+                    />
+                </a>
+            </div>
+            `;
+         })
+        .join('');
+    
+    gallery.insertAdjacentHTML("beforeend", markup);
+}
