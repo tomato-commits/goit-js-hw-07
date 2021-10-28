c960fc8ba4f2e0d6f845d36c8b4a067c0a924d75
tomato-commits <arthurpeter7@gmail.com>
2021-10-28 19:54:25 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener("click", onImageClick);
+createGalleryItems();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
+function createGalleryItems() {
+    const markup = galleryItems
+        .map((image) => {
+            return `
+                <a class="gallery__item" href="${image.original}">
+                    <img
+                        class="gallery__image"
+                        src="${image.preview}" 
+                        alt="${image.description}"
+                    />
+                </a>
+            `;
+         })
+        .join('');
+    
+    gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+function onImageClick(event) {
+    // prevent link from opening
+    event.preventDefault();
+}
